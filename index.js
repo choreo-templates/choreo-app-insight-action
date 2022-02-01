39e495b8ec850e748ae2dc817ac9f33cbe525e9d
Chamod Perera <chamodp@wso2.com>
2022-02-01 13:47:03 +0530

    App Insight Action Done

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const AppInsights = require("applicationinsights");
+const core = require("@actions/core");
 
-const key = process.env.APP_INSIGHT_KEY;
+try {
+    const appInsightKey = core.getInput("appInsightKey");
+    const vscodeChoreoUserEmail = core.getInput("vscodeChoreoUserEmail")
+    const vscodeChoreoUserIdpId = core.getInput("vscodeChoreoUserEmail")
 
-AppInsights.setup(key)
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true, true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .setSendLiveMetrics(false)
-    .setDistributedTracingMode(AppInsights.DistributedTracingModes.AI)
-    .start();
+    AppInsights.setup(appInsightKey)
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true, true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true)
+        .setUseDiskRetryCaching(true)
+        .setSendLiveMetrics(false)
+        .setDistributedTracingMode(AppInsights.DistributedTracingModes.AI)
+        .start();
 
-const AppInsightsClient = AppInsights.defaultClient;
+    const AppInsightsClient = AppInsights.defaultClient;
 
-const isWSO2User = process.env.VSCODE_CHOREO_USER_EMAIL
-    ? process.env.VSCODE_CHOREO_USER_EMAIL.endsWith("@wso2.com")
-    : false;
+    const isWSO2User = vscodeChoreoUserEmail
+        ? vscodeChoreoUserEmail.endsWith("@wso2.com")
+        : false;
 
-const eventObject = {
-    name: "editor-workspace-git-push",
-    properties: {
-        scope: "git-push",
-        idpId: process.env.VSCODE_CHOREO_USER_IDP_ID
-            ? process.env.VSCODE_CHOREO_USER_IDP_ID
-            : "",
-        isWSO2User: isWSO2User ? "true" : "false",
-    },
-};
-AppInsightsClient.trackEvent(eventObject);
+    const eventObject = {
+        name: "editor-workspace-git-push",
+        properties: {
+            scope: "git-push",
+            idpId: vscodeChoreoUserIdpId
+                ? vscodeChoreoUserIdpId
+                : "",
+            isWSO2User: isWSO2User ? "true" : "false",
+        },
+    };
+    AppInsightsClient.trackEvent(eventObject);
+} catch (e) {
+    core.setOutput("choreo-app-insight-status", "failed");
+    core.setFailed(e.message);
+    console.log("choreo-app-insight-status", "failed");
+    console.log(e.message);
+}
+
